0ad227d0c484e1ba19c31c9327c61a8b7352cdfc
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-07-28 18:33:07 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 $msg = "";
+$login_username = "";
+$password_username = "";
 include "config.php";
 include "server.php";
 ob_start();
@@ -16,7 +18,9 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta charset="utf-8">
     <title>Kyqja</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
@@ -29,18 +33,24 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
                     <div class="card-body">
                         <?php
                         if (!empty($msg)) {
-                            echo '<p>' . $msg . '</p>';
+                            echo '
+                <div class="alert alert-danger d-flex align-items-center" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                    <div>
+                       ' . $msg . '
+                    </div>
+                </div>';
                         }
                         ?>
                         <h4 class="card-title">Kyquni n&euml; panelin menagjues</h4>
                         <form method="POST" class="my-login-validation" action="#">
                             <div class="form-group">
                                 <label for="email">P&euml;rdoruesi</label>
-                                <input id="email" type="text" class="form-control" placeholder="P&euml;rdoruesi" name="username" value="" required autofocus="" oninvalid="this.setCustomValidity('Ju lutem shkruani p&euml;rdoruesin');" oninput="this.setCustomValidity('');">
+                                <input id="email" type="text" class="form-control" placeholder="P&euml;rdoruesi" name="username" value="<?= $login_username ?>" required autofocus="" oninvalid="this.setCustomValidity('Ju lutem shkruani p&euml;rdoruesin');" oninput="this.setCustomValidity('');">
                             </div>
                             <div class="form-group">
                                 <label for="password">Fjal&euml;kalimi
-                                    <input id="password" type="password" placeholder="Fjal&euml;kalimi" class="form-control" name="password" required data-eye oninvalid="this.setCustomValidity('Ju lutem shkruani fjal&euml;kalimin');" oninput="this.setCustomValidity('');">
+                                    <input id="password" type="password" placeholder="Fjal&euml;kalimi" class="form-control" value="<?= $password_username ?>" name="password" required data-eye oninvalid="this.setCustomValidity('Ju lutem shkruani fjal&euml;kalimin');" oninput="this.setCustomValidity('');">
                             </div>
                             <div class="form-group m-0">
                                 <button type="submit" name="login_submit" class="btn btn-primary btn-block">
