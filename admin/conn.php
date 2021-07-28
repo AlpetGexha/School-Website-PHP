d53863331e9af4dad15529a0c9074b644b748fe2
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-07-28 18:29:56 +0200

    DB with PDO  for visitor_info

--- admin/conn.php
+++ admin/conn.php
@@ -1,22 +1,17 @@
 <?php
 
-### EDIT HERE ###
-
-// DB CONNECT INFO
-$db_host = "localhost";
-$db_name = "blog_admin_db";
-$db_user = "root";
-$db_pw = "";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "nexhmedinnixha";
 
 // DB TABLE INFO
-$GLOBALS['hits_table_name'] = "page_hits";
 $GLOBALS['info_table_name'] = "visitor_info";
 
-### STOP EDITING HERE ###
 
 // CONNECT TO DB
 try {
-	$GLOBALS['db'] = new PDO("mysql:host=".$db_host.";dbname=".$db_name, $db_user, $db_pw, array(PDO::ATTR_PERSISTENT => false, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false));
+	$GLOBALS['db'] = new PDO("mysql:host=". $servername.";dbname=".$db_name, $username, $password, array(PDO::ATTR_PERSISTENT => false, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false));
 }
 catch(PDOException $e) {
     echo $e->getMessage();
