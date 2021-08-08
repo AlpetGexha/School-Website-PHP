6f5c75dce57411a79a9faf81ba332bd3f0fab14d
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-08-09 01:15:59 +0200

    Update single.php

--- single.php
+++ single.php
@@ -17,7 +17,7 @@ $c_row = $c_result->fetch_assoc();
 ?>
 <?php get_header($c_row['emri']); ?>
 
-<?php 
+<?php
 require_once 'admin/conn.php';
 updateInfo();
 ?>
@@ -53,38 +53,24 @@ updateInfo();
         </div>
 
         <div class="col-2">
-            <div class="card m-auto first-card	" style="width: 18rem;padding: 20px; background: #FCFCFC;">
-                <h5 class="card-title text-center p-2"><b>Informatika</b></h5>
-                <div class="user-media media mb-4">
-                    <p>
-                        &Euml;sht&euml; profil n&euml; kuad&euml;r t&euml; drejtimit t&euml; Elektroteknik&euml;s.
-                        Brenda k&euml;tij profili nx&euml;n&euml;sit mund t&euml; m&euml;sojn&euml; p&euml;r: <br>
 
-                        Aplikacionet e Office-it,<br>
-                        Sistemet Operative,<br>
-                        Rrjetet Kompjuterike,<br>
+            <?php if ($c_row['colum_table'] != null) : ?>
+                <div class="card m-auto first-card	" style="width: 18rem;padding: 20px; background: #FCFCFC;">
+                    <h5 class="card-title text-center p-2"><b><?= $c_row['emri'] ?></b></h5>
+                    <div class="user-media media mb-4">
+                        <p>
+                            <?= $c_row['colum_table'] ?>
+                        </p>
+                    </div>
 
-                        Algoritme,<br>
-                        Baza t&euml; t&euml; dh&euml;nave,<br>
-                        Gjuh&euml; Programuese (C++),<br>
-                        Web disejn,<br>
-                        Arkitektur&euml; Kompjuteri.<br><br>
-
-                        Viti i Ri shkollor …
-                        Drejtimi i Informatik&euml;s n&euml; vitin e ri shkollor, 2021 fillon i p&euml;rgatitur sa i p&euml;rket gadishm&euml;ris&euml; s&euml; kabineteve p&euml;r t’iu sh&euml;rbyer nx&euml;n&euml;sve sa m&euml; mir&euml;.
-                        N&euml; k&euml;t&euml; kontekst vlen&euml; t&euml; theksohet se ekzistojn&euml; dy kabinete t&euml; paisur me kompjuter&euml; solid&euml; p&euml;r pun&euml;. Ata jan&euml; t&euml; lidhur n&euml; rrjetin e Internetit, k&euml;shtuq&euml; nx&euml;n&euml;sit mund t&euml; shfryt&euml;zojn&euml; informacione edhe nga Interneti.
-
-                        P&euml;r t&euml; shfryt&euml;zuar materiale p&euml;r m&euml;sim, klikoni m&euml; posht&euml; !
-                    </p>
                 </div>
-
-            </div><br>
+            <?php endif; ?>
+            <br>
             <?php include "assets/php/colum_table.php"; ?>
         </div>
     </div>
 
 </div>
-</div>
-</div>
+
 <br> <br>
 <?php get_footer(); ?>
